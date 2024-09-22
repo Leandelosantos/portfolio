7435c71bc3d17f2c37d7fe70879e255908b75ad1
Leandelosantos <leandrodelosantos@hotmail.com>
2024-09-22 03:57:37 -0300

    test img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -43,7 +43,7 @@ const WrapperHeader = styled.header`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  background-image: url("../src/assets/moon1.jpg");
+  background-image: url("../assets/moon1.jpg");
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
